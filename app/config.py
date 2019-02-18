b7b779c89e9db0843973a0d4430f84446fc8f1b9
wecode <umurangamclaire@gmail.com>
2019-02-18 15:49:42 +0200

    adding categories of news in views.py

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-     MOVIE_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    MOVIE_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={} &apiKey={}'
 
 
 
--- app/views.py
+++ app/views.py
@@ -3,11 +3,44 @@ from app import app
 
 # Views
 @app.route('/')
-def news():
+def index():
 
     '''
     View root page function that returns the news page and its data
     '''
 
-    title = 'Home - Welcome to The best Movie Review Website Online'
+    title = 'Home - Welcome to The best News Review Website Online'
     return render_template('news.html', title = title)
+
+    from .request import get_news
+
+@app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting top news
+    top_news = get_news('top')
+    print(top_news)
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title,top = top_news)
+  
+  # Getting sports news
+    breaking_news = get_news('top')
+    print(top_news)
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title,top = top_news)
+
+# Getting business news
+    breaking_news = get_news('top')
+    print(top_news)
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title,top = top_news)
+
+# Getting health news
+    breaking_news = get_news('top')
+    print(top_news)
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title,top = top_news)
