c02842ba6bcde6f969259df5e0522cf253de0344
wecode <umurangamclaire@gmail.com>
2019-02-19 12:53:32 +0200

    added category for article

--- app/request.py
+++ app/request.py
@@ -1,11 +1,14 @@
 from app import app
 import urllib.request,json
 from .models import source
+from .models import article
 
 Source = source.Source
+Article = article.Article
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
 
 # Getting the source base url
 base_url = app.config["SOURCE_API_BASE_URL"]
@@ -37,6 +40,33 @@ def process_results(source_list):
     return source_results
 
 
+def process_results(article_list):
+    '''
+    Function  that processes the article result and transform them to a list of Objects
+
+    Args:
+        article_list: A list of dictionaries that contain article details
+
+    Returns :
+        article_results: A list of article objects
+    '''
+    article_results = []
+    for article_item in article_list:
+       autho = article_item.get('author')
+        title = article_item.get('title')
+        description= article_item.get('description')
+        url =article_item.get('url')
+        urlToImage = article_item.get('urlToImage')
+        publishedAt = article_item.get('publishedAt')
+        content = article_item.get('content')
+
+        if url:
+            artical_object =Article(author,title,description, url, urlToImage,publishedAt,content)
+            article_results.append(article_object)
+
+    return article_results
+
+
 def get_sources(category):
     '''
     Function that gets the json response to our url request
@@ -54,6 +84,27 @@ def get_sources(category):
             source_results = process_results(source_results_list)
 
     return source_results
+
+def get_articles(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_articles_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        article_results = None
+
+        if get_articles_response['articles']:
+            article_results_list = get_articles_response['articles']
+            article_results = process_results(article_results_list)
+
+    return article_results
+
+
+
 
 def get_source(id):
     get_sources_details_url = base_url.format(id,api_key)
@@ -76,3 +127,19 @@ def get_source(id):
             source_object = Source(id,name,description,url,category,language,country)
 
     return source_object
+
+
+def search_source(source_name):
+    search_source_url = 'https://api.thesourcedb.org/3/search/source?api_key={}&query={}'.format(api_key,source_name)
+    
+    with urllib.request.urlopen(search_source_url) as url:
+        search_source_data = url.read()
+        search_source_response = json.loads(search_source_data)
+
+        search_source_results = None
+        if search_source_response['results']:
+            search_source_list = search_source_response['results']
+            search_source_results = process_results(search_source_list)
+
+
+    return search_source_results
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template 
+from flask import render_template,request,redirect,url_for
 from app import app 
-from .request import get_sources, get_source
+from .request import get_sources, get_source, search_source
 
 # Views
 @app.route('/')
@@ -16,15 +16,35 @@ def index():
     sports_news = get_sources('sports')
     health_news = get_sources('health')
     title = 'Home - Welcome to The best News Review Website Online'
-    return render_template('index.html', title = title, general = general_news, business = business_news, sports = sports_news ,health = health_news )
+    return render_template('index.html',title = title, general = general_news, business = business_news, sports = sports_news ,health = health_news )
 
-@app.route('/source/<int:id>')
+    title = 'Home - Welcome to The best News Review Website Online'
+
+    search_source = request.args.get('source_query')
+
+    if search_source:
+        return redirect(url_for('search',source_name=search_source))
+    else:
+        return render_template('index.html',title = title, general = general_news, business = business_news, sports = sports_news ,health = health_news )
+
+@app.route('/source/<id>')
 def source(id):
 
     '''
     View source page function that returns the source details page and its data
     '''
-    source = get_movie(id)
-    title = f'{source.title}'
+    source = get_sources(id)
+    name = f'{source.name}'
 
-    return render_template('source.html',title = title,source = source)
+    return render_template('source.html',name = name,source = source)
+
+@app.route('/search/<source_name>')
+def search(source_name):
+    '''
+    View function to display the search results
+    '''
+    source_name_list = source_name.split(" ")
+    source_name_format = "+".join(source_name_list)
+    searched_sources = search_source(source_name_format)
+    title = f'search results for {source_name}'
+    return render_template('search.html',sources = searched_sources)
